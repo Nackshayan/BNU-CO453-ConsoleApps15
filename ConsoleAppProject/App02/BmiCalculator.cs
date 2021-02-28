5b8039e9de9af78c79e10d0eb8917ab265ac4ed0
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2021-02-28 23:04:05 +0000

    test images added and bmi meanings added

--- ConsoleAppProject/App02/BmiCalculator.cs
+++ ConsoleAppProject/App02/BmiCalculator.cs
@@ -26,12 +26,20 @@ namespace ConsoleAppProject.App02
         public const int CENTIMETERS_IN_METRES = 100;
         public const int INCHES_IN_FEET = 12;
 
+        public const string UNDERWEIGHT = "Underweight";
+        public const string NORMAL = "Normal";
+        public const string OVERWEIGHT = "Overweight";
+        public const string OBESE_CLASS_ONE = "Obese Class One";
+        public const string OBESE_CLASS_TWO = "Obese Class Two";
+        public const string OBESE_CLASS_THREE = "Obese Class Three";
+
         private double weight;
         private double height;
         private string unitType;
         private string weightUnit;
         private string heightUnit;
         private double bmi;
+        private object bmiMeaning;
 
         /// <summary>
         /// BMi Calculator Constructor
@@ -56,10 +64,10 @@ namespace ConsoleAppProject.App02
 
             unitType = SelectUnitType(" Please select between Imperical and Metric units > ");
 
-            Console.WriteLine($"\n Calculating BMI in {unitType}");
+            Console.WriteLine($"\n Calculating BMI from {weightUnit} and {heightUnit}");
 
-            weightUnit = InputWeight($"\n Please enter the weight in {weightUnit} > ");
-            heightUnit = InputHeight($"\n Please enter the height  in {heightUnit} > ");
+            weight = InputWeight($"\n Please enter the weight in {weightUnit} > ");
+            height = InputHeight($"\n Please enter the height  in {heightUnit} > ");
 
             CalculateBmi();
             OutputBmi();
@@ -68,7 +76,37 @@ namespace ConsoleAppProject.App02
         private void OutputBmi()
         {
             Console.WriteLine($"\n {weight} {weightUnit}" +
-                $" is {height} {heightUnit}<br/>Your BMI is {bmi}!\n");
+                $" is {height} {heightUnit}");
+            Console.WriteLine($"Your BMI is {bmi}!\n");
+            Console.WriteLine($"This means you are {bmiMeaning}!");
+        }
+
+        private void BmiMeaning()
+        {
+            if(bmi <= 18.50)
+            {
+                bmiMeaning = UNDERWEIGHT;
+            }
+            else if(bmi >= 18.5 && bmi <= 24.9)
+            {
+                bmiMeaning = NORMAL;
+            }
+            else if(bmi >= 25.0 && bmi <= 29.9)
+            {
+                bmiMeaning = OVERWEIGHT;
+            }
+            else if(bmi >= 30.0 && bmi <= 34.9)
+            {
+                bmiMeaning = OBESE_CLASS_ONE
+            }
+            else if(bmi >= 35 && bmi <= 39.9)
+            {
+                bmiMeaning = OBESE_CLASS_TWO
+            }
+            else if(bmi >= 40)
+            {
+                bmiMeaning = OBESE_CLASS_THREE;
+            }
         }
 
         /// <summary>
@@ -79,11 +117,14 @@ namespace ConsoleAppProject.App02
             if(weightUnit == KILOGRAMS && heightUnit == METRES)
             {
                 bmi = weight / (height * height);
+               
             }
             else if(weightUnit == STONE && heightUnit == FEET)
             {
                 bmi = ((weight * POUNDS_IN_STONE) * 703) / ((height * INCHES_IN_FEET) * (height * INCHES_IN_FEET));
             }
+
+            bmi = Convert.ToInt32(bmi);
         }
 
         /// <summary>
@@ -109,10 +150,14 @@ namespace ConsoleAppProject.App02
         {
             if (choice.Equals("1"))
             {
+                heightUnit = METRES;
+                weightUnit = KILOGRAMS;
                 return METRIC;
             }
             else if (choice.Equals("2"))
             {
+                heightUnit = FEET;
+                weightUnit = STONE;
                 return IMPERIAL;
             }
             else
