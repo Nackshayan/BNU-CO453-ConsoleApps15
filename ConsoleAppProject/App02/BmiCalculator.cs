0751aafd5582e9b6d2ac873f32aba5ba566d687a
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2021-02-28 21:46:03 +0000

    retried calculator

--- ConsoleAppProject/App02/BmiCalculator.cs
+++ ConsoleAppProject/App02/BmiCalculator.cs
@@ -14,111 +14,101 @@ namespace ConsoleAppProject.App02
     {
         // constants
         public const string STONE = "Stone";
-        public const string POUNDS = "Pounds";
         public const string KILOGRAMS = "Kilograms";
 
         public const string FEET = "Feet";
-        public const string INCHES = "Inches";
-        public const string CENTIMETERS = "Centimeters";
-
-        public const string METRIC = "metric";
+        public const string METRES = "Metres";
+    
+        public const string METRIC = "Metric";
         public const string IMPERIAL = "Imperial";
-           
 
-        // attributes
+        public const int POUNDS_IN_STONE = 14;
+        public const int CENTIMETERS_IN_METRES = 100;
+        public const int INCHES_IN_FEET = 12;
+
+        private double weight;
+        private double height;
         private string unitType;
         private string weightUnit;
         private string heightUnit;
-        private int calculation;
-        private int weight;
-        private int height;
         private double bmi;
 
         /// <summary>
-        /// Constructor for BmiConverter
+        /// BMi Calculator Constructor
         /// </summary>
         public BmiCalculator()
         {
-            unitType = null;
-            weightUnit = null;
-            heightUnit = null;
-            weight = 0;
-            height = 0;
+            bmi = 0;
+            weight = 0.0;
+            height = 0.0;
+
+            unitType = METRIC;
+            weightUnit = KILOGRAMS;
+            heightUnit = METRES;
         }
 
         /// <summary>
-        /// This method will output the Body Mass Index
-        /// based on the data provided by the user.
+        /// 
         /// </summary>
         public void CalculateBodyMassIndex()
         {
             OutputHeading();
 
-            unitType = SelectUnit(" Please select between Metric and Imperial > ");
-                
-            Console.WriteLine($"\n Calculating BMI using {unitType}");
+            unitType = SelectUnitType(" Please select between Imperical and Metric units > ");
 
-            InputWeight($"\n Please enter your weight in {weightUnit} > ");
-            InputHeight($"\n Please enter your height in {heightUnit} > ");
+            Console.WriteLine($"\n Calculating BMI in {weightUnit} and {heightUnit}");
+
+            weightUnit = InputWeight($"\n Please enter the weight in {weightUnit} > ");
+            heightUnit = InputHeight($"\n Please enter the height in {heightUnit}")
 
             CalculateBmi();
-            OutputBodyMassIndex();
+            OutputBmi();
+        }
+
+        private void OutputBmi()
+        {
+            Console.WriteLine($"\n {weight} {weightUnit}" +
+                $" is {height} {heightUnit}<br/>Your BMI is {bmi}!\n");
         }
 
         /// <summary>
-        /// To calculate the Body Mass Index based on the
         /// 
         /// </summary>
-        /// <returns></returns>
-        private double CalculateBmi()
+        private void CalculateBmi()
         {
+            if(weightUnit == KILOGRAMS && heightUnit == METRES)
             {
-                if (unitType == METRIC)
-                {
-                    weightUnit = KILOGRAMS;
-                    heightUnit = CENTIMETERS;
-
-                   calculation = weight / ((height / 100) ^ 2);
-
-                    bmi = Convert.ToInt32(calculation);
-                }
-                else if (unitType == IMPERIAL)
-                {
-                    weightUnit = STONE;
-                    heightUnit = FEET;
-
-                    calculation = (weight * 703) / (height ^ 2);
-
-                    bmi = Convert.ToInt32(calculation);
-                }
+                bmi = weight / (height * height);
+            }
+            else if(weightUnit == STONE && heightUnit == FEET)
+            {
+                bmi = ((weight*POUNDS_IN_STONE)*703)/((height*INCHES_IN_FEET)* (height * INCHES_IN_FEET))
             }
         }
 
         /// <summary>
-        /// Display a menu of distance units and then
-        /// prompt the user to select one and return it.
+        /// 
         /// </summary>
         /// <param name="prompt"></param>
         /// <returns></returns>
-        private string SelectUnit(string prompt)
+        private string SelectUnitType(string prompt)
         {
             string choice = DisplayChoices(prompt);
 
-            string unit = ExecuteChoice(choice);
-            Console.WriteLine($"\n You have chosen {unit}");
-            return unit;
+            unitType = ExecuteChoice(choice);
+            Console.WriteLine($"\n You have chosen {unitType}");
+            return unitType;
         }
 
         /// <summary>
-        /// Returns the selected unit of distance
+        /// 
         /// </summary>
         /// <param name="choice"></param>
         /// <returns></returns>
-        private static string ExecuteChoice(string choice)
+        private string ExecuteChoice(string choice)
         {
-
             if (choice.Equals("1"))
-            {       
+            {
                 return METRIC;
             }
             else if (choice.Equals("2"))
@@ -134,13 +124,11 @@ namespace ConsoleAppProject.App02
         }
 
         /// <summary>
-        /// This method displays the options to the user
-        /// and prompts the user to select an option for
-        /// both the from unit and to unit.
+        /// 
         /// </summary>
         /// <param name="prompt"></param>
         /// <returns></returns>
-        private static string DisplayChoices(string prompt)
+        private string DisplayChoices(string prompt)
         {
             Console.WriteLine();
             Console.WriteLine($" 1. {METRIC}");
@@ -153,32 +141,27 @@ namespace ConsoleAppProject.App02
         }
 
         /// <summary>
-        /// Prompt the user to enter their weight
-        /// Input the weight as a whole number
-        /// </summary>  
-        private double InputWeight(string prompt)
-        {
-            Console.Write(prompt);
-            string value = Console.ReadLine();
-            return Convert.ToInt32(value);
-        }
-
+        /// 
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <returns></returns>
         private double InputHeight(string prompt)
         {
             Console.Write(prompt);
             string value = Console.ReadLine();
-            return Convert.ToInt32(value);
+            return Convert.ToDouble(value);
         }
 
-    /// <summary>
-    /// This method will output the dstance calculated
-    /// by the conversion methods
-    /// </summary>  
-    private void OutputBodyMassIndex()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <returns></returns>
+        private double InputWeight(string prompt)
         {
-            Console.WriteLine($"\n {weight} {weightUnit}" +
-                $"  {height} {heightUnit}\n");
-            Console.WriteLine($"\n {bmi}!");
+            Console.Write(prompt);
+            string value = Console.ReadLine();
+            return Convert.ToDouble(value);
         }
 
         /// <summary>
